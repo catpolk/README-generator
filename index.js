85de011c129fa6af9f1a62dd11a4c669579ffed4
Catherine <cpolknytx@gmail.com>
2022-10-02 22:09:24 -0500

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+
+// const generateREADME = 
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Please enter a disription of your project",
+        name: "Discription"
+    },
+    {
+        type: "input",
+        massage: "Please provide installation instructuins",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        massage: "Please provide usage information",
+        name: "Usage"
+    },
+    {
+        type: "input",
+        massage: "Please provide contribution guidelines",
+        name: "License"
+    },
+    {
+        type: "input",
+        massage: "Please provide test instructions",
+        name: "Test"
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
