59cd0270a19396c5fefed24d08f76bd1b4138228
Catherine <cpolknytx@gmail.com>
2022-10-04 15:35:56 -0500

    added license links and badges

--- index.js
+++ index.js
@@ -3,34 +3,39 @@ const inquirer = require('inquirer');
 
 const fs = require('fs');
 
-// const generateREADME = 
+const generateMarkdown = require(`./utils/generateMarkdown`);
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+    {
         type: "input",
-        message: "Please enter a disription of your project",
+        message: "Enter a disription of your project",
         name: "Discription"
     },
     {
         type: "input",
-        massage: "Please provide installation instructuins",
+        massage: "Provide installation instructuins",
         name: "Installation"
     },
     {
         type: "input",
-        massage: "Please provide usage information",
+        massage: "Provide usage information for your project",
         name: "Usage"
     },
     {
         type: "input",
-        massage: "Please provide contribution guidelines",
+        massage: "Provide contribution guidelines",
         name: "Contribution"
     },
     {
         type: "list",
-        message: "Choose a license?",
+        message: "Choose a license for your project?",
         name: "License",
         choices: ["None", "MIT License", "Apache License 2.0", "GNU General Public License v3.0" ],
       },
@@ -43,16 +48,24 @@ const questions = [
 ];
 
 
-
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${data.name.toLowerCase().split(' ').join('')}.json`)
+    
+    fs.writeFile(fileName, JSON.stringify(data), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+}
+
+
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then(function(response) {
         console.log(response)
-    })
+        var fileContent = generateMarkdown(response);
+        writeToFile(fileContent)
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,87 @@
+const index = require('../index.js');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = '';
+    if (badge === 'MIT License')
+      badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+    } else if (license === 'Apache 2.0') {
+      badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  } else if (license === 'GNU v3.0') {
+      badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  } else {
+    badge = ""
+  }
+  return badge;
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+    if(license === 'MIT') {
+      licenseLink = 'https://choosealicense.com/licenses/mit/'
+    } else if (license === 'Apache 2.0') {
+      licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0'
+    } else if (license === 'GPL v3.0') {
+      licenseLink = 'https://www.gnu.org/licenses'
+    } else {
+      licenseLink = ""
+    }
+    return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if(license === 'None') {
+    licenseSection = ''
+  } else {
+    licenseSection =
+    `License: ${license} `
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## ${renderLicenseSection(responce.license)} ${renderLicenseBadge(responce.license)}
+  ### ${renderLicenseLink(responce.license)}
+
+  ## Table of Contents:
+  ###  * [Discription](#discription)
+  ###  * [Installation](#installation)
+  ###  * [Usage](#usage)
+  ###  * [Contributors](#contributors)
+  ###  * [License](#license)
+  ###  * [Tests](#tests)
+
+  ##Discription:
+  ### Provide a discription:
+  ### ${responce.discription}
+  
+  ## Installation:
+  ### You must install the following for this app to function:
+  ### ${responce.installation}
+  
+  ## Usage:
+  ### ${responce.usage}
+  
+  ## Contributors:
+  ### ${responce.contributions}
+  
+  ## Tests:
+  ### Run the following commands in your terminal to test this app:
+  ### ${responce.tests}
+  
+  ### Github: https://github.com/${responce.askMe}
+  ### or
+  ### Email: ${responce.email}
 
 `;
 }
